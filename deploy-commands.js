a15971aa7cca502896db4b9580c3a93e2bace43e
t0mmygit <tommychinliwei@gmail.com>
2024-08-25 01:23:43 +0800

    configuration and update for docker.

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,7 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
+require('dotenv').config();
 
 const commands = [];
 // Grab all the command folders from the commands directory you created
@@ -33,7 +33,7 @@ for (const folder of commandFolders) {
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST().setToken(token);
+const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
 // and deploy your commands!
 (async () => {
@@ -42,7 +42,7 @@ const rest = new REST().setToken(token);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, process.env.DISCORD_GUILD_ID),
 			{ body: commands },
 		);
 
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, ActivityType, Embed } = require('discord.js');
-const { token } = require('./config.json');
-const { EmbedBuilder } = require('discord.js');
+require('dotenv').config();
 
 const client = new Client({ 
 	intents: [
@@ -81,4 +80,4 @@ client.on(Events.InteractionCreate, async interaction => {
 	}
 });
 
-client.login(token);
+client.login(process.env.DISCORD_TOKEN);
