c2605b8b4de660af4200860f68f5cd312c224ff7
t0mmygit <tommychinliwei@gmail.com>
2024-08-29 18:05:59 +0800

    define project constants.

---
+++ constant.js
@@ -0,0 +1,9 @@
+const { ActivityType } = require('discord.js');
+
+module.exports = {
+    PREFIX: '?hey',
+    BOT_STATUS: 'idle',
+    ACTIVITY_TYPE: ActivityType.playing,
+    ACTIVITY_NAME: 'discord.js',
+    GUILD_LOG_CHANNEL_ID: '1236677665042468935',
+};
