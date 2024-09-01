744c4adc7ac2e42686e8b7d394ee617f85a07b60
t0mmygit <tommychinliwei@gmail.com>
2024-09-01 22:30:34 +0800

    Added embeds color constants.

--- constant.js
+++ constant.js
@@ -6,4 +6,8 @@ module.exports = {
     ACTIVITY_TYPE: ActivityType.playing,
     ACTIVITY_NAME: 'discord.js',
     GUILD_LOG_CHANNEL_ID: '1236677665042468935',
+    COLOR_ERROR: 0xFF0000,
+    COLOR_SUCCESS: 0x00FF00,
+    COLOR_INFO: 0x0000FF,
+    COLOR_SECONDARY: 0x808080,
 };
