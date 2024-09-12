f90048922ab2368ef8d3f135d6ebf7502f7fe4da
t0mmygit <tommychinliwei@gmail.com>
2024-09-12 22:47:16 +0800

    References user to inventory.

--- services/InventoryService.js
+++ services/InventoryService.js
@@ -6,15 +6,18 @@ class InventoryService {
         this.inventory = Inventory;
     }
 
-    async createInventory(value, user) {
+    async createInventory(value, userId) {
         try {
             const inventory = await this.inventory.create(value);
 
-            inventory.setUser(User.findOne({ where: { discordId: user.id} }));
+            // TODO: UserService Instead
+            const user = await User.findOne({ where: { discord_id: userId }});
+
+            inventory.setUser(user);
 
             return inventory;
         } catch (error) {
-            // TODO: Handle in other file
+            // TODO: Handle in other file | Proper error handling?
             console.error(`Error creating inventory: ${error.message}`);
         }
     }
