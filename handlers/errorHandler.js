7aa1e62528cf66493a458e24e6d7206bb2d23ef0
t0mmygit <tommychinliwei@gmail.com>
2024-09-26 00:45:43 +0800

    Handle timeout error separately.

--- handlers/errorHandler.js
+++ handlers/errorHandler.js
@@ -1,18 +1,22 @@
-const { EmbedBuilder } = require('@discordjs/builders');
+const { EmbedBuilder } = require('discord.js');
 const client = require('@/index.js').client;
 const { basename } = require('node:path');
+const { COLOR_ERROR } = require('@/constant.js');
+const { wrapInBold, wrapInCodeBlock } = require('@utils');
+
+// TODO: Handle 'deleted' collector error.
 
 async function handleError(error, fileName) {
+    if (isTimeoutError(error)) return;
 
     const errorEmbed = new EmbedBuilder()
         .setTitle(basename(fileName))
         .setFields(
             {
-                name: error.code,
-                value: `Message: ${error.message}\n` + `Name: ${error.name}\n`, 
+                name: error.code || 'Unknown Error Code',
+                value: constructErrorEmbedFieldValue(error),
             }
         ); 
-    console.error(error);
 
     let channel;
     const channelId = process.env.ERROR_LOGS_CHANNEL_ID;
@@ -33,4 +37,44 @@ async function handleError(error, fileName) {
     }
 }
 
-module.exports = handleError;
+function handleEmbedTimeoutError(error, response) { 
+    if (!isTimeoutError(error)) return;
+
+    const timeoutEmbed = EmbedBuilder.from(response.embeds[0]).setColor(COLOR_ERROR);
+
+    response.edit({
+        content: wrapInBold('Timed out! Please try again.'),
+        embeds: [timeoutEmbed],
+        components: [],
+    });
+}
+
+async function logError(error, response, fileName) {
+    handleEmbedTimeoutError(error, response);
+    await handleError(error, fileName);
+}
+
+function isTimeoutError(error) {
+    return error.message.includes('time') && error.code === 'InteractionCollectorError';
+}
+
+function constructErrorEmbedFieldValue(error) {
+    console.error(error);
+
+    let errors = [];
+    const { code, message, stack } = error;
+
+    if (code) errors.push(`${wrapInCodeBlock('code', 'single')} ${code}`);
+    if (message) errors.push(`${wrapInCodeBlock('message', 'single')} ${message}`);
+    if (stack) errors.push(`${wrapInCodeBlock('stack', 'single')} ${stack}`);
+
+    // TODO: Handle getOwnProperties errors.
+    
+    return errors.join('\n');
+}
+
+module.exports = {
+    handleError,
+    handleEmbedTimeoutError,
+    logError,
+};
