4421cf7e68ea2484ed2c232cca3226adaf4b55e0
t0mmygit <tommychinliwei@gmail.com>
2024-10-04 00:49:51 +0800

    Added new Guild model. Shorten logs for models/index.

--- models/index.js
+++ models/index.js
@@ -9,18 +9,14 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-console.log('Environment:', env);
-console.log('Connecting to database...');
-
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  // sequelize = new Sequelize(config.database, config.username, config.password, config);
   sequelize = new Sequelize(config);
 }
 
-console.log('Connected to database!');
+console.log(`Current environment is ${env}. Connected to database!`);
 
 fs
   .readdirSync(__dirname)
