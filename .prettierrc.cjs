84044296d50838f2d01b1ffaafe2d579295a198b
t0mmygit <tommychinliwei@gmail.com>
2025-01-19 13:49:43 +0800

    chore: add prettier local configuration.

---
+++ .prettierrc.cjs
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+    printWidth: 80,
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    trailingComma: "es5",
+    bracketSpacing: true,
+    proseWrap: "preserve",
+};
+
+module.exports = config;
