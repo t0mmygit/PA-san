f1b91935caa9506058c748f3f3534808991de34b
t0mmygit <tommychinliwei@gmail.com>
2025-01-22 00:51:42 +0800

    style(prettier): add semicolons.

--- handlers/clientSingletonHandler.js
+++ handlers/clientSingletonHandler.js
@@ -1,23 +1,24 @@
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.GuildMessageReactions,
-        GatewayIntentBits.MessageContent
-    ]
+        GatewayIntentBits.MessageContent,
+    ],
 });
 
 client.commands = new Collection();
 client.prefixCommands = new Collection();
 
-client.login(process.env.DISCORD_TOKEN)
+client
+    .login(process.env.DISCORD_TOKEN)
     .then(() => {
         console.log(`[${new Date().toLocaleString()}] Client logged in!`);
     })
     .catch(() => {
-        console.error('Client login failed! Check your token!');
+        console.error("Client login failed! Check your token!");
     });
 
 module.exports = client;
