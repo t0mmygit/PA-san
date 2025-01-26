23985823c6c73002f99cf7672e039ccf9bfb757b
t0mmygit <tommychinliwei@gmail.com>
2025-01-27 00:35:11 +0800

    refactor(handler): improve and simplify error handling logic
    Improved?
    - Code readability by encapsulating each section into their own
      functions, thus, minimizing clutter for the main function.
    
    Simplified?
    - Remove functions that were not being called.
    - Combined logic into one main function.

--- handlers/errorHandler.js
+++ handlers/errorHandler.js
@@ -1,99 +1,87 @@
-const { EmbedBuilder, bold, inlineCode, codeBlock } = require('discord.js');
-const { basename } = require('node:path');
-const { COLOR_ERROR } = require('@/constant.js');
-const { subtext } = require('@discordjs/formatters');
-const webhook = require('@handlers/webhookHandler');
+const {
+    DiscordAPIError,
+    DiscordjsError,
+    DiscordjsErrorCodes,
+    EmbedBuilder,
+} = require("discord.js");
+const { basename } = require("node:path");
+const { COLOR_ERROR } = require("@/constant.js");
+const { inlineCode, codeBlock, subtext } = require("@discordjs/formatters");
+const webhook = require("@handlers/webhookHandler");
 
-// TODO: Handle 'deleted' collector error.
+// TODO: Handle 'deleted' collector error. (When collecting, user/some sort event that cause the message to be deleted.)
+async function handleError(error, file, response = "none") {
+    console.error("Error Handled:", error);
+    if (error instanceof DiscordjsError) {
+        handleDiscordjsError(error, response);
+    }
 
-async function handleError(error, fileName) {
-    if (isTimeoutError(error)) return;
+    if (error instanceof DiscordAPIError) {
+        handleDiscordAPIError(error, response);
+    }
 
-    console.error('Error Handled:', error);
+    const errorEmbed = constructErrorEmbed(error, file);
+    await webhook.send({ embeds: [errorEmbed] });
+}
+
+function handleDiscordjsError(error, response) {
+    if (error.code === DiscordjsErrorCodes.InteractionCollectorError) {
+        console.log("Interaction Collector Error (Not Handled Yet).");
+    }
+}
+
+async function handleDiscordAPIError(error, response) {
+    if (error.code == 50013) {
+        const message = await response.channel.send({
+            content: `Insufficient permissions! Please contact the server administrator. \n${subtext("Message will be deleted in 5 seconds.")}`,
+        });
+
+        setTimeout(() => message.delete(), 5000);
+    }
+}
+
+function readableTimestamp() {
     const options = {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        hour12: true
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: true,
     };
-    const humanReadableTimestamp = new Date().toLocaleString('en-US', options);
 
-    const errorEmbed = new EmbedBuilder()
+    return new Date().toLocaleString("en-US", options);
+}
+
+function constructErrorEmbed(error, file) {
+    return new EmbedBuilder()
         .setColor(COLOR_ERROR)
-        .setTitle(basename(fileName))
-        .setFields(
-            {
-                name: error.code || 'Unknown Error Code',
-                value: constructErrorEmbedFieldValue(error) || 'No error definition.',
-            }
-        ) 
-        .setFooter({ text: `Issued on ${humanReadableTimestamp}` });
-
-        await webhook.send({ embeds: [errorEmbed] });
-}
-
-function handleEmbedTimeoutError(error, response) { 
-    if (!isTimeoutError(error)) return;
-
-    const timeoutEmbed = EmbedBuilder.from(response.embeds[0]).setColor(COLOR_ERROR);
-
-    response.edit({
-        content: bold('Timed out! Please try again.'),
-        embeds: [timeoutEmbed],
-        components: [],
-    });
-}
-
-async function handlePermissionError(error, response, filename, reason = error.code) {
-    if (!isPermissionError(error)) {
-        await handleError(error, filename);
-
-        return;
-    } 
-    console.error('Missing Permission Error!');
-
-    const message = await response.channel.send({
-        content: `[${reason}] No permission! Please contact the server moderator. \n${subtext('Message will be deleted in 5 seconds.')}`,
-    });
-
-    setTimeout(() => message.delete(), 5000);
-}
-
-async function logError(error, response, fileName) {
-    handleEmbedTimeoutError(error, response);
-    await handleError(error, fileName);
-}
-
-function isTimeoutError(error) {
-    return error.message.includes('time') && error.code === 'InteractionCollectorError';
-}
-
-function isPermissionError(error) {
-    return error.message.includes('Missing Permissions') && error.code == 50013;
+        .setTitle(basename(file))
+        .setFields({
+            name: error.code || "Unknown Error Code",
+            value:
+                constructErrorEmbedFieldValue(error) || "No error definition.",
+        })
+        .setFooter({ text: `Issued on ${readableTimestamp()}` });
 }
 
 function constructErrorEmbedFieldValue(error) {
     let errors = [];
 
     if (!error) {
-        return 'No error definition.';
+        return "No error definition.";
     }
 
-    if (error.code) errors.push(inlineCode('code') + error.code);
-    if (error.message) errors.push(inlineCode('message') + error.message);
+    if (error.code) errors.push(inlineCode("code") + error.code);
+    if (error.message) errors.push(inlineCode("message") + error.message);
     if (error.stack) errors.push(codeBlock(error.stack));
 
     // TODO: Handle getOwnProperties errors.
-    
-    return errors.join('\n');
+
+    return errors.join("\n");
 }
 
 module.exports = {
     handleError,
-    handleEmbedTimeoutError,
-    handlePermissionError,
-    logError,
 };
