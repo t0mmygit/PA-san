57128d4c4b1021d80cd17137689bf70ea4d28f79
t0mmygit <tommychinliwei@gmail.com>
2025-01-27 21:47:07 +0800

    feat(qualify): add ability to non-qualified messages
    - Refactored part of the code to improve readability.
    (e.g. if (!await functionName(...)) )

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,24 +1,19 @@
-const {
-    Events,
-    PermissionFlagsBits,
-    EmbedBuilder,
-    bold,
-} = require("discord.js");
-const { subtext, userMention } = require("@discordjs/formatters");
+const { Events, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
+const { bold, subtext, userMention } = require("@discordjs/formatters");
 const { Channel } = require("@models");
-const {
-    handleError,
-    handlePermissionError,
-} = require("@handlers/errorHandler");
-const { COLOR_SUCCESS } = require("@/constant.js");
+const { handleError } = require("@handlers/errorHandler");
+const { COLOR_SUCCESS, COLOR_ERROR } = require("@/constant.js");
+const channelSchema = require("@schemas/channel-settings");
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 module.exports = (client) => {
     client.on(Events.MessageReactionAdd, async (reaction, user) => {
+        if (user.bot) return;
+
         try {
             await handleDeleteByReaction(client, reaction, user);
-            await handleQualifiedByReaction(client, reaction, user);
+            await handleQualifiedByReaction(reaction, user);
         } catch (error) {
             await handleError(error, __filename);
         }
@@ -26,45 +21,27 @@ module.exports = (client) => {
 };
 
 async function handleDeleteByReaction(client, reaction, user) {
-    // Check for "Delete" reaction
     if (!isDeleteReaction(reaction.emoji.name)) return;
 
-    // Check if server delete by reaction is enabled
-    if (!(await allowDeleteByReaction(reaction.message.channelId, user))) {
-        await handleReactionError(
-            reaction,
-            user,
-            `${userMention(user.id)}, delete by reaction is not enabled! Please contact the server moderator. \n${subtext("Message will be deleted in 5 seconds.")}`,
-            5000
-        );
+    const canReact = await canDeleteByReaction(reaction.message.channelId);
+    if (!canReact) {
+        await handleReactionError(reaction, user, "delete-by-reaction");
 
         return;
     }
 
     // Check if reacted message was requested by the same user
     // Reason: The message can only be deleted by the user who originally requested it.
-    const interactionUserId = getinteractionUserID(reaction.message);
-    if (!(await extractedUserIdMatchesAuthor(interactionUserId, user.id))) {
-        await handleReactionError(
-            reaction,
-            user,
-            `${userMention(user.id)}, you did not requested this render! \n${subtext("Message will be deleted in 3 seconds.")}`
-        );
+    const interactionUserId = extractInteractionUserID(reaction.message);
+    if (!userIdMatchesAuthor(interactionUserId, user.id)) {
+        await handleReactionError(reaction, user, "user-requested-not-match");
 
         return;
     }
 
-    /*
-     * 1. Check if reacted message is owned by a bot.
-     * 2. Check if reacted message matches this bot's id.
-     * 3. Then, deletes reacted message.
-     */
-    if (
-        reaction.message.author.bot &&
-        reaction.client.application.id === client.application.id
-    ) {
+    if (isReactionByBotAndSameClient(client, reaction)) {
         const response = await reaction.message.reply({
-            content: `${userMention(user.id)}, you've requested to delete this message... \n${subtext("Please wait for a few seconds.")}`,
+            content: constructUserResponse(user.id, "message-delete-request"),
         });
 
         await delay(3000);
@@ -75,117 +52,116 @@ async function handleDeleteByReaction(client, reaction, user) {
     }
 }
 
-async function handleQualifiedByReaction(client, reaction, user) {
-    if (!isQualifiedReaction(reaction.emoji.name)) return;
-    if (!isAdministrator(reaction, user)) return;
+async function handleQualifiedByReaction(reaction, user) {
+    const reactionName = reaction.emoji.name;
 
-    if (!(await allowQualifyByReaction(reaction.message.channelId, user))) {
-        await handleReactionError(
-            reaction,
-            user,
-            `${userMention(user.id)}, qualify by reaction is not enabled! \n${subtext("Message will be deleted in 5 seconds.")}`,
-            5000
-        );
+    if (!isValidQualifiedReaction(reactionName)) return;
+
+    const isAdmin = await isAdministrator(reaction, user);
+    if (!isAdmin) return;
+
+    const canReact = await canQualifyByReaction(reaction.message.channelId);
+    if (!canReact) {
+        await handleReactionError(reaction, user, "qualify-by-reaction");
 
         return;
     }
 
-    await qualifyMessage(reaction.message);
-
-    // TODO: Remove triggering reaction.
-}
-async function handleReactionError(
-    reaction,
-    user,
-    content,
-    miliseconds = 3000
-) {
-    try {
-        const response = await reaction.message.reply({
-            content: content,
-        });
-        await delay(miliseconds);
-        response.delete();
-
-        await reaction.users.remove(user);
-    } catch (error) {
-        await handlePermissionError(
-            error,
-            reaction.message,
-            __filename,
-            "Reaction deletion"
-        );
-    }
-}
-
-async function channelHasPermission(channelId, object) {
-    const channel = await Channel.findOne({
-        where: { discord_channel_id: channelId },
-    });
-
-    if (channel === null) return false;
-
-    const settings = JSON.parse(channel.settings);
-
-    // TODO: Check if object exist or not. (Joi schema object)
-    return settings.value[object];
-}
-
-async function allowDeleteByReaction(channelId, user) {
-    try {
-        const channelPermission = await channelHasPermission(
-            channelId,
-            "allowDeleteByReaction"
-        );
-
-        return channelPermission && !user.bot;
-    } catch (error) {
-        await handleError(error, __filename);
-    }
-}
-
-async function allowQualifyByReaction(channelId, user) {
-    try {
-        const channelPermission = await channelHasPermission(
-            channelId,
-            "allowQualifyByReaction"
-        );
-
-        return channelPermission && !user.bot;
-    } catch (error) {
-        await handleError(error, __filename);
-    }
-}
-
-async function qualifyMessage(message) {
+    const message = reaction.message;
     if (!message.editable) {
         console.log("Message is not editable by client user.");
     }
 
     const embed = EmbedBuilder.from(message.embeds[0]);
+    if (isQualifiedReaction(reactionName)) {
+        await message.edit({
+            content: bold(`Qualified for ${getCurrentMonth()} Pixlr Contest!`),
+            embeds: [embed.setColor(COLOR_SUCCESS)],
+        });
+    }
 
-    await message.edit({
-        content: bold(`Qualified for ${getCurrentMonth()} Pixlr Contest!`),
-        embeds: [embed.setColor(COLOR_SUCCESS)],
-    });
+    if (isNonQualifiedReaction(reactionName)) {
+        await message.edit({
+            content: bold(
+                `Not Qualified for ${getCurrentMonth()} Pixlr Contest!`
+            ),
+            embeds: [embed.setColor(COLOR_ERROR)],
+        });
+    }
+
+    await reaction.users.remove(user.id);
 }
 
-async function extractedUserIdMatchesAuthor(extractedUserId, userId) {
-    if (!extractedUserId) return false;
+async function handleReactionError(reaction, user, content) {
+    try {
+        const response = await reaction.message.reply({
+            content: constructUserResponse(user.id, content),
+        });
+        await delay(3000);
+        response.delete();
 
-    return extractedUserId == userId;
+        await reaction.users.remove(user);
+    } catch (error) {
+        await handleError(error, __filename, response);
+    }
 }
 
-function isQualifiedReaction(emojiName) {
-    return emojiName === "✅";
+async function canDeleteByReaction(channelId) {
+    return await hasChannelPermission(channelId, "allowDeleteByReaction");
 }
 
-function isNotQualifiedReaction(emojiName) {
-    return emojiName === "❌";
+async function canQualifyByReaction(channelId) {
+    return await hasChannelPermission(channelId, "allowQualifyByReaction");
 }
 
-function isDeleteReaction(emojiName) {
-    return emojiName === "🗑️";
+/* Handle 'is' condition */
+async function isAdministrator(reaction, user) {
+    // TODO: Check if user has permission or allowed to qualify (user role choose)
+    const member = await reaction.message.guild.members.fetch(user.id);
+
+    return member.permissions.has(PermissionFlagsBits.Administrator);
+}
+
+function isQualifiedReaction(emoji) {
+    return emoji === "✅";
+}
+
+function isNonQualifiedReaction(emoji) {
+    return emoji === "❌";
+}
+
+function isDeleteReaction(emoji) {
+    return emoji === "🗑️";
+}
+
+function isValidQualifiedReaction(emoji) {
+    return isQualifiedReaction(emoji) || isNonQualifiedReaction(emoji);
+}
+
+function isReactionByBotAndSameClient(client, reaction) {
+    return (
+        reaction.message.author.bot &&
+        reaction.client.application.id === client.application.id
+    );
+}
+
+/* Other functions */
+async function hasChannelPermission(channelId, object) {
+    try {
+        const channel = await Channel.findOne({
+            where: { discord_channel_id: channelId },
+        });
+
+        if (channel === null) return false;
+
+        const schemaMetadata = channelSchema.describe();
+        console.log("Schema Metadata: ", schemaMetadata);
+        // TODO: Check if object exist or not. (Joi schema object)
+        const settings = JSON.parse(channel.settings);
+        return settings.value[object];
+    } catch (error) {
+        await handleError(error, __filename);
+    }
 }
 
 function getCurrentMonth() {
@@ -194,14 +170,32 @@ function getCurrentMonth() {
     return date.toLocaleString("default", { month: "long" });
 }
 
-async function isAdministrator(reaction, user) {
-    // TODO: Check if user has permission or allowed to qualify (user role choose)
-    const member = await reaction.message.guild.members.fetch(user.id);
+function getContent(content) {
+    switch (content) {
+        case "qualify-by-reaction":
+            return "Qualify by reaction is not enabled!";
 
-    return member.permissions.has(PermissionFlagsBits.Administrator);
+        case "delete-by-reaction":
+            return "Delete by reaction is not enabled!";
+
+        case "user-requested-not-match":
+            return "You did not request for this render!";
+
+        case "message-delete-request":
+            return "You have requested to delete this message.";
+
+        default:
+            return "Something went wrong, please report to the developer.";
+    }
 }
 
-function getinteractionUserID(message) {
+function userIdMatchesAuthor(extractedUserId, userId) {
+    if (!extractedUserId) return false;
+
+    return extractedUserId == userId;
+}
+
+function extractInteractionUserID(message) {
     const embed = message.embeds[0];
 
     if (!embed === null || !embed?.image === undefined) return null;
@@ -211,4 +205,11 @@ function getinteractionUserID(message) {
     const userId = cleanUrl.split("/").pop();
 
     return userId;
+}
+
+function constructUserResponse(userId, content) {
+    const user = `${userMention(userId)}`;
+    const sub = `\n${subtext("This message will be deleted in 3 seconds.")}`;
+
+    return `${user} ${getContent(content)}${sub}`;
 }
