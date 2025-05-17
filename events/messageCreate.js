dcf22293bde9982712ed4ae95e8be5d40c2538e4
t0mmygit <tommychinliwei@gmail.com>
2025-05-17 23:31:14 +0800

    feat(events): Remove logic and stop listening for messageCreate event.

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,52 +1,14 @@
-const { Events } = require('discord.js');
-const { PREFIX } = require('@/constant.js');
-const { removeOngoingCommand } = require('@/handlers/storageHandler.js');
-const { handleError } = require('@handlers/errorHandler');
-const { User, Guild } = require('@/models');
+const { Events } = require("discord.js");
+const { PREFIX } = require("@/constant.js");
+const { removeOngoingCommand } = require("@/handlers/storageHandler.js");
+const { handleError } = require("@handlers/errorHandler");
+const { User, Guild } = require("@/models");
 
 module.exports = (client) => {
-    client.on(Events.MessageCreate, async message => {
-        try {
-            if (await allowCommand(message)) {
-                if (!hasAccess(message)) return;
-            }
-            
-            const user = await User.getByDiscordId(message.author.id);
-
-            if (user === null) {
-                await require('@auth/userVerification').execute(message);
-
-                return;
-            }
-
-            const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
-
-            if (args[0].length === 0) {
-                message.reply('Please provide a command!');
-
-                // TODO: Show button for commands
-
-                return;
-            }
-
-            const action = args.shift().toLowerCase();
-            const command = client.prefixCommands.get(action);
-
-            if (!command) {
-                if (process.env.NODE_ENV === 'development') {
-                    console.log(`Message Error: Command '${action}' not found or does not exist.`);
-                }
-                
-                return;
-            }
-
-            await removeOngoingCommand(client, message, action);
-            await command.execute(client, message, args);
-        } catch (error) {
-            handleError(error, __filename);
-        }
+    client.on(Events.MessageCreate, async (message) => {
+        return;
     });
-}
+};
 
 function isAuthorizedUser(message) {
     if (!message.member) return false;
@@ -69,8 +31,12 @@ async function guildHasPrefixPermission(guildId) {
 }
 
 async function allowCommand(message) {
-    const hasPrefixPermission = await guildHasPrefixPermission(message.guild.id);
+    const hasPrefixPermission = await guildHasPrefixPermission(
+        message.guild.id
+    );
     return (
-        message.author.bot || !message.content.startsWith(PREFIX) || !hasPrefixPermission
-    )
-} 
+        message.author.bot ||
+        !message.content.startsWith(PREFIX) ||
+        !hasPrefixPermission
+    );
+}
