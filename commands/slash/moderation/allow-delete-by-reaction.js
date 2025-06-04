3d20a356bcb3435ae07f4a5fcd7a58d16129f205
t0mmygit <tommychinliwei@gmail.com>
2025-06-04 13:11:49 +0800

    refactor(permissions): Utilize 'SetDefaultMemberPermissions' instead of using user-defined middleware.

--- commands/slash/moderation/allow-delete-by-reaction.js
+++ commands/slash/moderation/allow-delete-by-reaction.js
@@ -4,33 +4,23 @@ const {
     bold,
 } = require("discord.js");
 const { Channel, Guild } = require("@models");
-const schema = require("@schemas/channel-settings");
 const { handleError } = require("@handlers/errorHandler");
-const userHasPermission = require("@middlewares/userHasPermission");
+const schema = require("@schemas/channel-settings");
 
 module.exports = {
-    middlewares: [userHasPermission([PermissionFlagsBits.Administrator])],
     data: new SlashCommandBuilder()
         .setName("allow-delete-by-reaction")
-        .setDescription(
-            "Allow to delete message by reaction for this channel."
-        ),
+        .setDescription("Allow to delete message by reaction for this channel.")
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
     async execute(interaction) {
         try {
             await interaction.deferReply({ ephemeral: true });
-
-            if (!isModerator(interaction.member)) {
-                await interaction.followUp({
-                    content: "You do not have permission to use this command!",
-                });
-                return;
-            }
 
             await setDeleteByReaction(interaction);
 
             await interaction.followUp({
                 content: bold(
-                    `Users can now delete messages from this bot using the '❌' reaction for this channel.`
+                    `Users can now delete messages from this bot using the '🗑️' reaction for this channel.`
                 ),
             });
         } catch (error) {
@@ -38,10 +28,6 @@ module.exports = {
         }
     },
 };
-
-function isModerator(member) {
-    return member.permissions.has(PermissionFlagsBits.Administrator);
-}
 
 async function setDeleteByReaction(interaction) {
     try {
